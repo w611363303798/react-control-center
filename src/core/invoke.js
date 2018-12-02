13c601214795e8b2c8a25d7bbeb2bbbce84d3308
nick.zhong2 <nick.zhong@boldseas.com>
2018-12-02 19:08:41 +0800

    invoke方法，增加method不存在的判断

--- src/core/invoke.js
+++ src/core/invoke.js
@@ -20,5 +20,10 @@ export default function (ccClassKey, ccInstanceKey, method, ...args) {
     return console.error(err.message);
   }
 
+  var fn = ref[method];
+  if(!fn){
+    const err = util.makeError(ERR.CC_CLASS_INSTANCE_METHOD_NOT_FOUND, vbi(` method:${method}`));
+    return console.error(err.message);
+  }
   ref[method].call(ref, ...args);
 }
